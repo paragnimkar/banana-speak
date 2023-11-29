67340c1d6a694faa58432e83ea3a5c4fbdcf8b4f
Parag Nimkar <nimkarparag5@gmail.com>
2023-11-29 19:14:38 +0530

    Input Output processing done

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector("#output");
+
+output.innerText = "Parag Nimkar";
 
 console.log(txtInput);
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input", txtInput.value);
+    output.innerText = "Let's translate " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler);
